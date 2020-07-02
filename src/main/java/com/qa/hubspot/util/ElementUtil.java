e71d986dba1fc0f4de4c32ff8ec41dc90b78cde1
Aman Gupta <amaangupta23@gmail.com>
2020-07-02 22:46:04 +0530

    POM code commit

--- src/main/java/com/qa/hubspot/util/ElementUtil.java
+++ src/main/java/com/qa/hubspot/util/ElementUtil.java
@@ -1,5 +1,34 @@
 package com.qa.hubspot.util;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class ElementUtil {
+
+	WebDriver driver;
+
+	public ElementUtil(WebDriver driver) {
+		this.driver = driver;
+
+	}
+
+	public WebElement getElement(By Locator) {
+		WebElement element = driver.findElement(Locator);
+		return element;
+
+	}
+
+	public void doSendKeys(By Locator, String value) {
+		getElement(Locator).sendKeys("value");
+	}
+
+	public void doClick(By Locator) {
+		getElement(Locator).click();
+	}
+
+	public String doGetText(By Locator) {
+		return getElement(Locator).getText();
+	}
 
 }

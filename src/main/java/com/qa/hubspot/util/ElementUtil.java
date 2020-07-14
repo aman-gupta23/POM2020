c4fac922502cd690c0428021c6d549dc8d1b01ee
Aman Gupta <amaangupta23@gmail.com>
2020-07-14 22:44:25 +0530

    POM code commit

--- src/main/java/com/qa/hubspot/util/ElementUtil.java
+++ src/main/java/com/qa/hubspot/util/ElementUtil.java
@@ -1,9 +1,15 @@
 package com.qa.hubspot.util;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ElementUtil {
@@ -37,13 +43,6 @@ public class ElementUtil {
 		return getElement(Locator).getText();
 	}
 
-	public String waitForTitleToBePresent(String title, int timeout) {
-		WebDriverWait wait = new WebDriverWait(driver, timeout);
-		wait.until(ExpectedConditions.titleContains(title));
-		return driver.getTitle();
-
-	}
-
 	public void clickWhenready(By Locator, int timeout) {
 		WebDriverWait wait = new WebDriverWait(driver, timeout);
 		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(Locator));
@@ -56,6 +55,119 @@ public class ElementUtil {
 		wait.until(ExpectedConditions.visibilityOf(element));
 		return element;
 
+	}
+
+	// **********************************Drop Down Utils
+	// *********************************
+
+	public void doSelectByVisibleText(By locator, String value) {
+		Select select = new Select(getElement(locator));
+		select.selectByVisibleText(value);
+	}
+
+	public void doSelectByIndex(By locator, int index) {
+		Select select = new Select(getElement(locator));
+		select.selectByIndex(index);
+	}
+
+	public void doSelectByValue(By locator, String value) {
+		Select select = new Select(getElement(locator));
+		select.selectByValue(value);
+	}
+
+	public int doDropDownOptionsCount(By locator) {
+		return doGetDropDownOptions(locator).size();
+	}
+
+	public ArrayList<String> doGetDropDownOptions(By locator) {
+		ArrayList<String> ar = new ArrayList<String>();
+		Select select = new Select(getElement(locator));
+		List<WebElement> OptionsList = select.getOptions();
+
+		for (int i = 0; i < OptionsList.size(); i++) {
+			String text = OptionsList.get(i).getText();
+			ar.add(text);
+		}
+		return ar;
+	}
+
+	public void doSelectDropDownValue(By locator, String value) {
+		Select selectday = new Select(getElement(locator));
+		List<WebElement> OptionsList = selectday.getOptions();
+
+		for (int i = 0; i < OptionsList.size(); i++) {
+			String text = OptionsList.get(i).getText();
+			if (text.equals(value)) {
+				OptionsList.get(i).click();
+				break;
+			}
+		}
+	}
+
+
+	// **********************************Actions class Utils
+	// *********************************
+
+	public void doDragAndDrop(By source, By target) {
+		Actions action = new Actions(driver);
+		WebElement sourceEle = getElement(source);
+		WebElement targetEle = getElement(target);
+		action.dragAndDrop(sourceEle, targetEle).build().perform();
+	}
+
+	public void doActionsSendKeys(By locator, String value) {
+		Actions action = new Actions(driver);
+		action.sendKeys(getElement(locator), value).build().perform();
+	}
+
+	public void doActionsClick(By locator) {
+		waitForElementPresent(locator, 10);
+		Actions action = new Actions(driver);
+		action.click(getElement(locator)).build().perform();
+	}
+
+	// ***************************** Wait Utils
+	// *******************************************
+
+	public List<WebElement> visibilityofAllElements(By locator, int timeout) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		return wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(locator));
+	}
+
+	public WebElement waitForElementPresent(By locator, int timeout) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		WebElement element = wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+		return element;
+	}
+
+	public WebElement waitForElementToBeClickable(By locator, int timeout) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+		return element;
+	}
+
+	public boolean waitForUrl(String url, int timeout) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		return wait.until(ExpectedConditions.urlContains(url));
+	}
+
+	public Alert waitForAlertToBePresent(int timeout) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+		return alert;
+	}
+
+	// clickWhenReady:
+	public void clickWhenReady(By locator, int timeout) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(locator));
+		element.click();
+	}
+
+	public String waitForTitleToBePresent(String title, int timeout) {
+		WebDriverWait wait = new WebDriverWait(driver, timeout);
+		wait.until(ExpectedConditions.titleContains(title));
+		return driver.getTitle();
 	}
 
 }

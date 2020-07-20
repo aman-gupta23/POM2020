0795f7e14a4bf1ab487fb21f1ba39e04df9234d7
Aman Gupta <amaangupta23@gmail.com>
2020-07-20 23:02:10 +0530

    POM code commit

--- src/main/java/com/qa/hubspot/base/BasePage.java
+++ src/main/java/com/qa/hubspot/base/BasePage.java
@@ -16,13 +16,15 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import com.qa.hubspot.util.ElementUtil;
+import com.qa.hubspot.util.OptionsManager;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BasePage {
 	WebDriver driver;
-	Properties prop;
+	public Properties prop;
 	public ElementUtil elementUtil;
+	public OptionsManager optionsManager;
 
 	public static ThreadLocal<WebDriver> tldriver = new ThreadLocal<WebDriver>();
 
@@ -31,17 +33,18 @@ public class BasePage {
 	}
 
 	public WebDriver init_driver(Properties prop) {
+		optionsManager = new OptionsManager(prop);
 		String browserName = prop.getProperty("browser");
 
 		if (browserName.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
 			// driver = new ChromeDriver();
-			tldriver.set(new ChromeDriver());
+			tldriver.set(new ChromeDriver(optionsManager.getChromeOptions()));
 
 		} else if (browserName.equalsIgnoreCase("firefox")) {
 			WebDriverManager.firefoxdriver().setup();
 			// driver = new FirefoxDriver();
-			tldriver.set(new FirefoxDriver());
+			tldriver.set(new FirefoxDriver(optionsManager.getFirefoxOptions()));
 
 		} else if (browserName.contentEquals("Safari")) {
 			WebDriverManager.getInstance(SafariDriver.class).setup();
@@ -68,8 +71,30 @@ public class BasePage {
 
 	public Properties init_prop() {
 		prop = new Properties();
+		String path = null;
+		String env = null;
+
 		try {
-			FileInputStream ip = new FileInputStream("src/main/java/com/qa/hubspot/config/config.properties");
+			env = System.getProperty("env");
+			System.out.println("env value is--->" + env);
+
+			if (env == null) {
+				path = "src/main/java/com/qa/hubspot/config/config.properties";
+			} else {
+				switch (env) {
+				case "qa":
+					path = "src/main/java/com/qa/hubspot/config/qa.config.properties";
+					break;
+				case "dev":
+					path = "src/main/java/com/qa/hubspot/config/dev.config.properties";
+					break;
+				default:
+					System.out.println("Invalid Value" + env);
+					break;
+				}
+			}
+
+			FileInputStream ip = new FileInputStream(path);
 			prop.load(ip);
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
@@ -84,7 +109,7 @@ public class BasePage {
 
 	public String getScreenshot() {
 		File src = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
-		String path = System.getProperty("user.dir") + "/Screenshot" + System.currentTimeMillis() + ".png";
+		String path = System.getProperty("user.dir") + "/Screenshots/" + System.currentTimeMillis() + ".png";
 		File destination = new File(path);
 		try {
 			FileUtils.copyFile(src, destination);
